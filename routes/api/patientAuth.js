7a4900eaad208de72afe7ba3cdff9c30c8be0a1a
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-12-02 03:19:15 +0200

    unify login

--- routes/api/patientAuth.js
+++ routes/api/patientAuth.js
@@ -1,48 +1,12 @@
 import express from 'express';
-import {UserCrud} from '../../models/userCrud.js';
-import jwt from 'jsonwebtoken';
+
 
 const server = express.Router();
-const SECRET = process.env.JWT_SECRET;
 
 server.post('/register', async (req, res, next) => {
     try {
         const user = await UserCrud.create(req.body);
         res.status(201).json(user);
-    } catch (error) {
-        console.log(error);
-        res.status(400).json({ message: error.message });
-    }
-});
-
-server.post('/login', async (req, res, next) => {
-    try {
-        const { email, password } = req.body;
-        const user = await UserCrud.readOne(email);
-        if (!user || user.password !== password || user.role !== 'PATIENT') {
-            return res.status(400).json({ message: 'Invalid credentials' });
-        }
-        const token = jwt.sign(
-            {
-              userId: user.id,
-              email: user.email,
-            },
-            SECRET,
-            { expiresIn: '1d' }
-        );
-        res.status(200).json({ 
-            token,
-            user: {
-                id: user.id,
-                firstName: user.firstName,
-                lastName: user.lastName,
-                phoneNumber: user.phoneNumber,
-                nationalId: user.nationalId,
-                dateOfBirth: user.dateOfBirth.toISOString().split('T')[0],
-                role: user.role,
-                email: user.email,
-            }
-        });
     } catch (error) {
         console.log(error);
         res.status(400).json({ message: error.message });
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import doctor from './api/doctorAuth.js';
 import user from './api/patientAuth.js';
 import specialization from './api/specialization.js';
+import login from './api/login.js';
 
 const router = express.Router();
 
@@ -9,6 +10,7 @@ router.get('/xx', (req, res) => {
     res.send('Hello World from router!');
 });
 
+router.use('/login', login);
 router.use('/doctor', doctor);
 router.use('/user', user);
 router.use('/', specialization);
