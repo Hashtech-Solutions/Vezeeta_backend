569bd931eee8fafd913978c7c4ec96514843bcf8
Steven Samir <106476257+stevensamir20@users.noreply.github.com>
2023-12-22 19:36:23 +0200

    dotenv

--- routes/api/login.js
+++ routes/api/login.js
@@ -1,62 +1,62 @@
-import { DoctorCrud } from '../../models/doctorCrud.js';
-import { UserCrud } from '../../models/userCrud.js';
-import jwt from 'jsonwebtoken';
-import express from 'express';
+import { DoctorCrud } from "../../models/doctorCrud.js";
+import { UserCrud } from "../../models/userCrud.js";
+import jwt from "jsonwebtoken";
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const server = express.Router();
 const SECRET = process.env.JWT_SECRET;
 
-server.post('/', async (req, res, next) => {
-    try {
-        const { email, password } = req.body;
-        const user = await UserCrud.readOne(email);
-        if (!user || user.password !== password) {
-            return res.status(400).json({ message: 'Invalid credentials' });
-        }
-        // Flatten the user object
-        if (user.role === 'DOCTOR') {
-            let doctor = await DoctorCrud.readOneMail(email);
-            doctor = { ...doctor, ...doctor.user };
-            delete doctor.user;
-            delete doctor.password;
-            const token = jwt.sign(
-                {
-                    userId: doctor.id,
-                    email: doctor.email,
-                },
-                SECRET,
-                { expiresIn: '1d' }
-            );
-            res.set('Authorization', token);
-            res.status(200).json(doctor);
-        }
-        else
-        {
-            const token = jwt.sign(
-                {
-                  userId: user.id,
-                  email: user.email,
-                },
-                SECRET,
-                { expiresIn: '1d' }
-            );
-            res.set('Authorization', token)
-            res.status(200).json({
-                id: user.id,
-                firstName: user.firstName,
-                lastName: user.lastName,
-                phoneNumber: user.phoneNumber,
-                nationalId: user.nationalId,
-                dateOfBirth: user.dateOfBirth.toISOString().split('T')[0],
-                role: user.role,
-                email: user.email,
-            });
-        }
-        
-    } catch (error) {
-        console.log(error);
-        res.status(400).json({ message: error.message });
+server.post("/", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await UserCrud.readOne(email);
+    if (!user || user.password !== password) {
+      return res.status(400).json({ message: "Invalid credentials" });
     }
+    // Flatten the user object
+    if (user.role === "DOCTOR") {
+      let doctor = await DoctorCrud.readOneMail(email);
+      doctor = { ...doctor, ...doctor.user };
+      delete doctor.user;
+      delete doctor.password;
+      const token = jwt.sign(
+        {
+          userId: doctor.id,
+          email: doctor.email,
+        },
+        SECRET,
+        { expiresIn: "1d" }
+      );
+      res.set("Authorization", token);
+      res.status(200).json(doctor);
+    } else {
+      const token = jwt.sign(
+        {
+          userId: user.id,
+          email: user.email,
+        },
+        SECRET,
+        { expiresIn: "1d" }
+      );
+      res.set("Authorization", token);
+      res.status(200).json({
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        phoneNumber: user.phoneNumber,
+        nationalId: user.nationalId,
+        dateOfBirth: user.dateOfBirth.toISOString().split("T")[0],
+        role: user.role,
+        email: user.email,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: error.message });
+  }
 });
 
 export default server;
--- server.js
+++ server.js
@@ -1,34 +1,36 @@
-import express from 'express';
-import routes from './routes/index.js';
+import express from "express";
+import routes from "./routes/index.js";
 import cors from "cors";
 import path from "path";
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
 
+dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const corsOptions = {
-    origin: true,
-    credentials: true
+  origin: true,
+  credentials: true,
 };
 
 app.use(express.json());
 app.set("trust proxy", 1);
 app.use(cors(corsOptions));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.use('/', routes);
+app.use("/", routes);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Example app listening on port ${PORT}!`);
 });
 
 export default app;
